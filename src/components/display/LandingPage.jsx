41189889750de5319cd511ee0401687329a1b9e0
anonymous <email>
2023-09-28 14:04:38 +0200

    changed background-color of cards; cards are clickable

--- src/components/display/LandingPage.jsx
+++ src/components/display/LandingPage.jsx
@@ -12,12 +12,28 @@ import DanceWithDragons from "../../assets/DanceWithDragons.png";
 import { NavLink } from "react-router-dom";
 
 export default function LandingPage() {
-    const {books} = useContext(Context);
+    const {books, setBooks2} = useContext(Context);
     const {num1, num2, num3} = useRandom();
+    const handleSearch = async (num) => {
+        try {
+          const response = await fetch(
+            `https://anapioficeandfire.com/api/books?name=${books[num]?.name}`
+          );
+    
+          if (!response.ok) {
+            throw new Error('No network response.');
+          }
+    
+          const data = await response.json();
+          setBooks2(data);
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      };
     return (
         <>
         
-            <div className="card">
+            <div className="card" onClick={() => handleSearch(num1)}>
                 <NavLink>
                     <img src={num1 === 0 ? GameOfThrones : num1 === 1 ? ClashOfKings : StormOfSwords} alt=""/>
                     <h3>{books[num1]?.name}</h3>
@@ -25,7 +41,7 @@ export default function LandingPage() {
                     <h4>$9,24</h4>
                 </NavLink>
             </div>
-            <div className="card">
+            <div className="card"  onClick={() => handleSearch(num2)}>
                 <NavLink>
                     <img src={num2 === 3 ? HedgeKnight : num2 === 4 ? FeastForCrows : num2 === 5 ? SwornSword : MysteryKnight} alt=""/>
                     <h3>{books[num2]?.name}</h3>
@@ -33,7 +49,7 @@ export default function LandingPage() {
                     <h4>$9,24</h4>
                 </NavLink>
             </div>
-            <div className="card">
+            <div className="card"  onClick={() => handleSearch(num3)}>
                 <NavLink>
                     <img src={num3 === 7 && DanceWithDragons}/>
                     <h3>{books[num3]?.name}</h3>
